10449f642e7041274802e1af69fa30815bef9d52
Njood <nejood03@gmail.com>
2025-02-14 19:23:06 +0300

    completed collections part 4 - using ArrayList

---
+++ Collections/src/arraylist.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val carBrands = arrayListOf("Toyota", "BMW", "Honda")
+    carBrands.add("Jeep")
+    carBrands.add("Ford")
+    carBrands.remove("Honda")
+
+    println(carBrands)
+}
